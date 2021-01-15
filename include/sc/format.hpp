eaa89f827a7ce52785d7a4e2532332b78676ed0c
Henrich Lauko <xlauko@mail.muni.cz>
2021-01-15 22:14:38 +0100

    fmt: add value formaters

---
+++ include/sc/format.hpp
@@ -0,0 +1,128 @@
+/*
+ * (c) 2021 Henrich Lauko <xlauko@mail.muni.cz>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#pragma once
+
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Value.h>
+#include <llvm/Support/raw_ostream.h>
+
+#include <sstream>
+#include <string>
+#include <string_view>
+
+namespace sc::fmt
+{
+    using split_view = std::pair< std::string_view, std::string_view >;
+
+    static inline split_view split( std::string_view p, char d, bool reverse = false ) noexcept
+    {
+        auto s = reverse ? p.rfind( d ) : p.find( d );
+        if ( s == p.npos )
+            return reverse ? split_view{ "", p } : split_view{ p, "" };
+        auto q = p;
+        q.remove_prefix( s + 1 );
+        p.remove_suffix( p.size() - s );
+        return { p, q };
+    }
+
+    template< typename gen_t, typename state_t > struct generator
+    {
+        using value_type = std::tuple_element_t< 0, std::result_of_t< gen_t( state_t ) > >;
+        gen_t _gen;
+        state_t _initial;
+
+        struct proxy
+        {
+            value_type v;
+            value_type *operator->() { return &v; }
+        };
+
+        struct iterator
+        {
+            gen_t _gen;
+            state_t _state;
+
+            iterator( gen_t g, state_t s ) : _gen( g ), _state( s ) {}
+            iterator &operator=( const iterator & ) = default;
+            iterator &operator++() { _state = std::get< 1 >( _gen( _state ) ); return *this; }
+            iterator operator++( int ) { auto x = *this; ++*this; return x; }
+            value_type operator*() { return std::get< 0 >( _gen( _state ) ); }
+            proxy operator->() { return { **this }; }
+            bool operator==( const iterator &o ) const { return _state == o._state; }
+            bool operator!=( const iterator &o ) const { return _state != o._state; }
+        };
+
+        auto begin() const { return iterator( _gen, _initial ); }
+        auto end()   const { return iterator( _gen, state_t() ); }
+
+        generator( gen_t gen, state_t init ) : _gen( gen ), _initial( init ) {}
+    };
+
+    static auto splitter( std::string_view s, char d, bool reverse = false )
+    {
+        return generator( [=]( auto _s ) { return split( _s, d, reverse ); }, s );
+    }
+
+    // trim from start (in place)
+    static inline void ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+            return !std::isspace(ch);
+        }));
+    }
+
+    // trim from end (in place)
+    inline void rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+            return !std::isspace(ch);
+        }).base(), s.end());
+    }
+
+    // trim from both ends (in place)
+    inline void trim(std::string &s) {
+        ltrim(s);
+        rtrim(s);
+    }
+
+    template< typename T >
+    inline std::string llvm_to_string( T val )
+    {
+        std::string buffer;
+        llvm::raw_string_ostream rso( buffer );
+        val->print( rso );
+        return buffer;
+    }
+
+    inline std::string llvm_name( llvm::Value * value )
+    {
+        if ( value->hasName() )
+            return value->getName().str();
+
+        auto str = llvm_to_string( value );
+        if ( llvm::isa< llvm::Instruction >( value ) ) {
+            str = std::string( *splitter( str, '=' ).begin() );
+            trim(str);
+        }
+
+        return str;
+    }
+
+    inline std::string llvm_name( llvm::Type * type )
+    {
+        return llvm_to_string( type );
+    }
+
+} // namespace lart
